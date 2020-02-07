c401e9fdeec65f49dc8be39959f65bd16fb8a9da
sattfatt <sattfatt@gmail.com>
2020-02-06 17:57:09 -0800

    made some minor changes

--- TimeCardSystem.py
+++ TimeCardSystem.py
@@ -26,37 +26,63 @@ time_interval = 1
 # initialize id
 ID = 0
 
+# end of day bool
+isEndOfDay = False
+
+time_between_reads = datetime.datetime.now()
+
+def end_of_day():
+    """
+    at the end of the day make sure to log the dictionary and reset the
+    dictionary.
+    """
+    fakelog()
+    global time_tables
+    time_tables = {}
+
+def fakelog():
+    print("fake logging.......Done")
+
+
+def time_table():
+
+    """This function listens for an RFID signal, reads it, and adds the ID,
+    data, and Time and appends it to the time_table dictionary. The dictionary
+    is indexed by a tuple (ID,name) where ID is an int and name is a string"""
+
+    global time_between_reads
+
+    time_difference = datetime.datetime.now() - time_between_reads
+
+    if time_difference.total_seconds() > time_interval:
+        # this should block until something is read.
+        ID, name = reader.read_fob()
+        name = ''.join(name.split())
+        time_between_reads = datetime.datetime.now()
+
+        # when we read we should keep note of the time between reads
+
+        if (ID, name) in time_tables:
+            time_tables[(ID, name)].append(datetime.datetime.now())
+
+        else:
+            time_tables[(ID, name)] = []
+            time_tables[(ID, name)].append(datetime.datetime.now())
+
+        # printing to the console
+        print("*************New Table***************")
+        for key, val in time_tables:
+            print(str(key) + " " +  str(val) + ":")
+            for t in time_tables[(key,val)]:
+                print(str(t))
+            print("------------------------------")
+
+
 # program start
 
 if __name__ == "__main__":
 
-    time_between_reads = datetime.datetime.now()
-
     while(True):
-
-        time_difference = datetime.datetime.now() - time_between_reads
-
-        if time_difference.total_seconds() > time_interval:
-            # this should block until something is read.
-            ID, name = reader.read_fob()
-            name = ''.join(name.split())
-            time_between_reads = datetime.datetime.now()
-
-            # when we read we should keep note of the time between reads
-
-            if (ID, name) in time_tables:
-                time_tables[(ID, name)].append(datetime.datetime.now())
-
-            else:
-                time_tables[(ID, name)] = []
-                time_tables[(ID, name)].append(datetime.datetime.now())
-
-            # printing to the console
-            print("*************New Table***************")
-            for key, val in time_tables:
-                print(str(key) + " " +  str(val) + ":")
-                for t in time_tables[(key,val)]:
-                    print(str(t))
-                print("------------------------------")
+        time_table()
 
 
