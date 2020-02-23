e64d6a09a1d65c99f35980eaf5581864f3a4741c
sattfatt <sattfatt@gmail.com>
2020-02-23 15:53:15 -0800

    added the log folder and the running time_tables log file

---
+++ tlogs/logviewer.py
@@ -0,0 +1,19 @@
+import pickle
+import sys
+
+def load_dict(path):
+    dictionary = pickle.load(open(path, "rb"))
+    return dictionary
+
+def print_dict(dictionary):
+    for key, val in dictionary.items():
+        print(key)
+        for tup in val:
+            print(tup)
+
+PATH = sys.argv[1]
+if(PATH):
+    dictionary = load_dict(PATH)
+    print_dict(dictionary)
+else:
+    print("Did not specify a file!")
